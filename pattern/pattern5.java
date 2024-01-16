ad7834f708d4efb1fc162fcb9d6239b72e1a2a88
mannatchopra1802 <mannatchopra1802>
2024-01-17 01:54:54 +0530

    Triangle Pattern1

---
+++ pattern/pattern5.java
@@ -0,0 +1,21 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class pattern5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while (i<=n) {
+            int j = 1;
+            while (j<=i) {
+                System.out.print(j + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        sc.close();
+    }
+}
